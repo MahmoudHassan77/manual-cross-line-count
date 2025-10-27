e448067f3a4b0bec74a18b5c1810def756405fd0
Mahmoud Hassan <mahmoudhassan7764@outlook.com>
2025-10-27 10:37:05 +0300

    f

--- src/App.js
+++ src/App.js
@@ -68,7 +68,7 @@ function App() {
     const hours = String(now.getHours()).padStart(2, '0');
     const minutes = String(now.getMinutes()).padStart(2, '0');
     const seconds = String(now.getSeconds()).padStart(2, '0');
-    const datetime = ${year}-${month}-${day} ${hours}:${minutes}:${seconds}; // YYYY-MM-DD HH:MM:SS
+    const datetime = `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`; // YYYY-MM-DD HH:MM:SS
     
     const newRecord = {
       lineName,
